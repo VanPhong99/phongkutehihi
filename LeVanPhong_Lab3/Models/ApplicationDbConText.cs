d0429e6b9dd48a182873be9303476003e8814a54
Administrator <Administrator@PC225>
2020-06-04 16:17:00 +0700

    Add project files.

---
+++ LeVanPhong_Lab3/Models/ApplicationDbConText.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LeVanPhong_Lab3.Models
+{
+    public class ApplicationDbConText : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDbConText()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        public static ApplicationDbConText Create()
+        {
+            return new ApplicationDbConText();
+        }
+    }
+}
